f0367d659b251a87b3803b8c1f16b41d3a633259
Edmar Dausan <edmar.dausan@netroadshow.com>
2023-07-05 00:32:23 +0800

    cra to vite conversion

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgrPlugin from 'vite-plugin-svgr'
+const path = require('path');
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // This changes the out put dir from dist to build
+  // comment this out if that isn't relevant for your project
+  build: {
+    outDir: 'build',
+  },
+  plugins: [
+    react(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+        // ...svgr options (https://react-svgr.com/docs/options/)
+      },
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    alias: {
+      "@": path.resolve(__dirname, "src/"),
+			"@components": path.resolve(__dirname, "src/components/"),
+			"@assets": path.resolve(__dirname, "src/assets/"),
+			"@api": path.resolve(__dirname, "src/api/"),
+			"@hooks": path.resolve(__dirname, "src/hooks/"),
+    }
+  }
+})
